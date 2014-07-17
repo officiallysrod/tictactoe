b3490710933df014ff9d16ed5157422970a07865
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-07-17 16:19:08 -0700

    renders the 3x3 board in the view using ng-repeat

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
 var tttApp = angular.module('TttApp', []);
 
 tttApp.controller('TTTController', function ($scope) {
+  
   $scope.test = "This is a test!";
 
   $scope.board = [
-    {
-      
-    }
+    [  
+      {cell: "aa", owner: ""},
+      {cell: "ab", owner: ""},
+      {cell: "ac", owner: ""}
+    ],
+    
+    [  
+      {cell: "ba", owner: ""},
+      {cell: "bb", owner: ""},
+      {cell: "bc", owner: ""}
+    ],
+    
+    [  
+      {cell: "ca", owner: ""},
+      {cell: "cb", owner: ""},
+      {cell: "cc", owner: ""}
+    ]
   ]
   
 });
