3ec0544a910cd3c28aa47f6abbb105ce67ac1c5b
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-07-18 16:27:29 -0700

    added a createSquares function that adds new cell objects to the board array when the page loads. added an init function to run the createSquares function on page load.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
 var tttApp = angular.module('TttApp', []);
 
-tttApp.controller('TTTController', function ($scope) {
+  tttApp.controller('TTTController', function ($scope) {
   
-  $scope.board = [
-    [  
-      {cell: "aa", owner: ""},
-      {cell: "ab", owner: ""},
-      {cell: "ac", owner: ""}
-    ],
+  $scope.board = []
+
+  // $scope.oldboard = [
+  //   [  
+  //     {cell: "aa", owner: "", pointValue: ""},
+  //     {cell: "ab", owner: "", pointValue: ""},
+  //     {cell: "ac", owner: "", pointValue: ""}
+  //   ],
     
-    [  
-      {cell: "ba", owner: ""},
-      {cell: "bb", owner: ""},
-      {cell: "bc", owner: ""}
-    ],
+  //   [  
+  //     {cell: "ba", owner: "", pointValue: ""},
+  //     {cell: "bb", owner: "", pointValue: ""},
+  //     {cell: "bc", owner: "", pointValue: ""}
+  //   ],
     
-    [  
-      {cell: "ca", owner: ""},
-      {cell: "cb", owner: ""},
-      {cell: "cc", owner: ""}
-    ]
-  ]
+  //   [  
+  //     {cell: "ca", owner: "", pointValue: ""},
+  //     {cell: "cb", owner: "", pointValue: ""},
+  //     {cell: "cc", owner: "", pointValue: ""}
+  //   ]
+  // ]
+
+  $scope.createSquares = function(numWidth) {
+    for(i = 0; i < numWidth * numWidth; i++){
+      $scope.board.push({owner: "", pointValue: ""});
+    }
+  }
 
   $scope.turn = 1;
 
@@ -28,22 +36,43 @@ tttApp.controller('TTTController', function ($scope) {
     if(cell.owner == ""){
       if($scope.turn == 1) {
         cell.owner = "X";
+        cell.pointValue = 1;
         $scope.turn = 2;
       }
       else {
         cell.owner = "O";
+        cell.pointValue = -1;
         $scope.turn = 1;
       }
     }
+    $scope.winChecker();
   }
 
-  $scope.setClass = function(cell) {
-    if(cell.owner == "x") {
-      return "xselected";
-    }
-    else {
-      return "oselected";
-    }
-  }
+//win checker variables
+  // $scope.row1 = $scope.board[0][0].pointValue + $scope.board[0][1].pointValue + $scope.board[0][2].pointValue;
+  // $scope.row2 = $scope.board[1][0].pointValue + $scope.board[1][1].pointValue + $scope.board[1][2].pointValue;
+  // $scope.row3 = $scope.board[2][0].pointValue + $scope.board[2][1].pointValue + $scope.board[2][2].pointValue;
+  // $scope.column1 = $scope.board[0][0].pointValue + $scope.board[1][0].pointValue + $scope.board[2][0].pointValue;
+  // $scope.column2 = $scope.board[0][1].pointValue + $scope.board[1][1].pointValue + $scope.board[2][1].pointValue;
+  // $scope.column3 = $scope.board[0][2].pointValue + $scope.board[1][2].pointValue + $scope.board[2][2].pointValue;
+  // $scope.diag1 = $scope.board[0][0].pointValue + $scope.board[1][1].pointValue + $scope.board[2][2].pointValue;
+  // $scope.diag1 = $scope.board[0][2].pointValue + $scope.board[1][1].pointValue + $scope.board[2][0].pointValue;
+
+  // $scope.winChecker = function(){
+  //   if($scope.row1 == 3){
+  //     alert("X wins!");
+  //   }
+  // }
+
+  // $scope.winChecker = function() {
+  //   if($scope.board[0][0].pointValue + $scope.board[0][1].pointValue + $scope.board[0][2].pointValue == 3){
+  //     alert("X wins!");
+  //   }
+  // }
+  var init = function(){
+    $scope.createSquares(3);
+  };
+
+  init();
 
 });
