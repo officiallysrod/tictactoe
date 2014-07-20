e540215d30e54f8e66b4c15811951a34c1b3e8c7
Shawn Rodriguez <shawn.w.rodriguez@gmail.com>
2014-07-19 20:57:11 -0700

    added a modal dialog box.  added ability for users to enter their names. restyled the page.

--- js/main.js
+++ js/main.js
@@ -12,11 +12,13 @@ var tttApp = angular.module('TttApp', []);
 
   $scope.scoreBoard = {xWins: 0, oWins: 0, ties: 0};
   $scope.turn = 1;
-  $scope.gameOver = "no";
+  $scope.gameOver = false;
   $scope.playCounter = 0;
+  $scope.playerOne = "Player One";
+  $scope.playerTwo = "Player Two";
 
   $scope.setChoice = function(cell) {
-    if($scope.gameOver === "no"){
+    if($scope.gameOver === false){
       if(cell.owner === ""){
         if($scope.turn === 1){
           cell.owner = "X";
@@ -33,83 +35,86 @@ var tttApp = angular.module('TttApp', []);
       }
       $scope.winChecker();
     }
+    else {
+      $scope.playAgain();
+    }
   }
 
   $scope.winChecker = function(){
     if($scope.board[0].pointValue + $scope.board[1].pointValue + $scope.board[2].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[0].pointValue + $scope.board[1].pointValue + $scope.board[2].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[3].pointValue + $scope.board[4].pointValue + $scope.board[5].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[3].pointValue + $scope.board[4].pointValue + $scope.board[5].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[6].pointValue + $scope.board[7].pointValue + $scope.board[8].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[6].pointValue + $scope.board[7].pointValue + $scope.board[8].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[0].pointValue + $scope.board[3].pointValue + $scope.board[6].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[0].pointValue + $scope.board[3].pointValue + $scope.board[6].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[1].pointValue + $scope.board[4].pointValue + $scope.board[7].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[1].pointValue + $scope.board[4].pointValue + $scope.board[7].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[2].pointValue + $scope.board[5].pointValue + $scope.board[8].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[2].pointValue + $scope.board[5].pointValue + $scope.board[8].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[0].pointValue + $scope.board[4].pointValue + $scope.board[8].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[0].pointValue + $scope.board[4].pointValue + $scope.board[8].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[2].pointValue + $scope.board[4].pointValue + $scope.board[6].pointValue === 3){
       $scope.scoreBoard.xWins = $scope.scoreBoard.xWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.board[2].pointValue + $scope.board[4].pointValue + $scope.board[6].pointValue === -3){
       $scope.scoreBoard.oWins = $scope.scoreBoard.oWins + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
     else if($scope.playCounter === 9){
       $scope.scoreBoard.ties = $scope.scoreBoard.ties + 1;
-      $scope.gameOver = "yes";
+      $scope.gameOver = true;
     }
   }
 
   $scope.playAgain = function(){
     $scope.board = [];
     $scope.createSquares(3);
-    $scope.gameOver = "no";
+    $scope.gameOver = false;
     $scope.turn = 1;
     $scope.playCounter = 0;
   }
